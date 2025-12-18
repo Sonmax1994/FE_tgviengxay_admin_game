18d9201840df07cadcc6ce12d286fe4e600919ac
LeCongLC <leconglc1996@gmail.com>
2025-12-18 22:35:02 +0700

    Update new endpoint for dev

--- src/app/core/services/http-service.service.ts
+++ src/app/core/services/http-service.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IApiService } from '../interfaces/api';
 import { IHttpClientOptions } from '../interfaces/http-client-options';
-const END_POINT_URL = (window as any).END_POINT_URL || 'https://api.tgviengxay.bet';
+declare let END_POINT_URL: any;
 @Injectable({
   providedIn: 'root'
 })
